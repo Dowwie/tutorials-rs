9b129e122b85c8ca8a2f410f5619a875f77e779c
dowwie <darinc@gmail.com>
2017-02-13 16:57:23 -0500

    moved authz tutorial into its own repo, created landing page

---
+++ docs/main.js
@@ -0,0 +1,13 @@
+function showBox(id) {
+	var activeElem = document.querySelector('.tutorial-box.active');
+	activeElem.classList.toggle('active');
+
+	var newBox = document.querySelector('#tutorial-box-' + id);
+	newBox.classList.toggle('active');
+
+	var activePic = document.querySelector('.tutorials-row img.active');
+	activePic.classList.toggle('active');
+
+	var picElem = document.querySelector('#pic' + id);
+	picElem.classList.toggle('active');
+}
